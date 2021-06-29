3a641b89b4f10670a83c16df5ec54e94c7ddc429
jamoozy <me@jamoozy.com>
2021-06-29 16:12:11 -0700

    Manifest v3 & popup mostly functional.

--- eventPage.js
+++ eventPage.js
@@ -1,29 +1,24 @@
-// All the keys we store.
-var keys = ["pbRate", "controls", "muted", "volume"];
-
-chrome.runtime.onMessage.addListener(
-  function messageListener(request, sender, sendResponse) {
-    console.log("Got message: %O  from sender: %O", request, sender);
-    if (sender.tab || request.req) {
-      // If sender.tab is true, this is a tab.  If request.req is truthy, then
-      // the popup is requesting initial data.  Either way, send the data.
-      console.log("Trying to get update to send.");
-      chrome.storage.local.get(keys,
-        function syncGet(items) {
-          console.log("Sending update: %O", items);
-          sendResponse(items);
-        }
-      );
-    } else {
-      // This is an update.
-      chrome.storage.local.set(request,
-        function syncSet() {
-          if (chrome.runtime.lastError) {
-            chrome.error("There was an error saving!");
-            chrome.error(chrome.runtime.lastError);
-          }
-        }
-      );
-    }
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log("Got message: %O  from sender: %O", request, sender);
+  if (sender.tab || request.req) {
+    // If sender.tab is true, this is a tab.  If request.req is truthy, then
+    // the popup is requesting initial data.  Either way, send the data.
+    console.log("Trying to get update to send.");
+    chrome.storage.sync.get(['state'], val => {
+      console.log("Sending update: %O", val.state);
+      sendResponse(val.state);
+    });
+    console.log("and now we wait for our promises to resolve ...");
+    return true;
+  } else {
+    // This is an update.
+    chrome.storage.sync.set({state: request}, () => {
+      if (chrome.runtime.lastError) {
+        chrome.error("There was an error saving!");
+        chrome.error(chrome.runtime.lastError);
+      } else {
+        console.log("successfully saved %O", request);
+      }
+    });
   }
-);
+});
--- popup.js
+++ popup.js
@@ -1,44 +1,70 @@
 var popup = (function popupScope() {
-  // The keys we send and update.
-  var keys = ["pbRate", "controls", "muted", "volume"];
-
-  // Gets an element by "key" (DOM ID).
-  function get(key) {
-    var e = document.getElementById(key);
-    if (!e) {
-      console.error('Could not get key "%s".', key);
-      return null;
-    }
-    return e
+  const opts = document.getElementsByClassName("option"); // Options for playback rate.
+  var state = {
+    playbackRate: 1.0,
+    controls: true,
+    muted: false,
+    volume: 85.0,
+    auto: false,
   }
 
   // Updates the form.
   function updateForm(response) {
-    console.log("Updating form with: %O", response);
-    for (var i = 0; i < keys.length; i++) {
-      if (keys[i]) {
-        console.log("Updating %s to %s", keys[i], response[keys[i]]);
-        get(keys[i]).setAttribute("value", response[keys[i]]);
+    console.log('response: %O', response);
+    if (!response) {
+      console.log('No previous data.');
+      return;
+    }
+
+    state = Object.assign(state, response);
+    console.log('state now: %O', state);
+
+    ////////////////////////
+    // update popup HTML
+
+    console.log('state.playbackRate:', state.playbackRate);
+    for (opt of opts) {
+      const v = Number(opt.attributes['x-attr-value'].value);
+      console.log('opt:%O --> %s', opt, v);
+      if (v === state.playbackRate) {
+        opt.classList.add('active-option');
+      } else {
+        opt.classList.remove('active-option');
       }
     }
+
+    document.getElementById('controls').checked = state.controls;
+    document.getElementById('muted').checked = state.muted;
+    document.getElementById('auto').checked = state.auto;
+    document.getElementById('volume').attributes['value'] = state.volume;
   }
 
   // Sends the form.
-  function sendForm(event) {
-    var msg = {};
-    for (var i = 0; i < keys.length; i++) {
-      msg[keys[i]] = document.getElementById(keys[i]).getAttribute("value");
+  function saveForm() {
+    console.log('saveForm()');
+    console.log("Sending: %O", state);
+    chrome.runtime.sendMessage(state);
+  }
+
+  function setPlaybackRate(ev) {
+    for (opt of opts) {
+      opt.classList.remove('active-option');
     }
-    console.log("Sending: %O", msg);
-    chrome.runtime.sendMessage(msg);
+    ev.target.classList.add('active-option');
+    state.playbackRate = Number(ev.target.attributes['x-attr-value'].value);
+    saveForm()
   }
 
   return {
     // Initialize the popup.
-    init: function init(event) {
-      console.log("init");
+    init: ev => {
       chrome.runtime.sendMessage({req: true}, updateForm);
-      get("submit").onclick = sendForm;
+      for (opt of opts) {
+        opt.onclick = setPlaybackRate;
+      }
+
+      //chrome.scripting.executeScript({
+      //  target: {
     },
   };
 })();
